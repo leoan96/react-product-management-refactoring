2f30ebe5a392fa21d126ca78986e56149f093035
Mun Kit <leoan96@hotmail.com>
2021-09-19 23:00:28 +0800

    implement add product functionality

---
+++ pages/products/add-product.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import AddProduct from "../../components/products/AddProduct";
+
+const AddProductPage = () => {
+  return (
+    <div>
+      <div className="container">
+        <h1>Add Product Page</h1>
+        <AddProduct />
+      </div>
+    </div>
+  );
+};
+
+export default AddProductPage;
