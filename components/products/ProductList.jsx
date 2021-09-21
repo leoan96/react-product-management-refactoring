6dcc2dbb23a70b22d6ff95bab57a965dbe36b3d5
Mun Kit <leoan96@hotmail.com>
2021-09-21 17:00:01 +0800

    sorting by type is now works

--- components/products/ProductList.jsx
+++ components/products/ProductList.jsx
@@ -1,15 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import Link from "next/link";
-import { Table, Tag, Space } from "antd";
-import ProductItem from "./ProductItem";
+import { Table, Tag, Space, Button } from "antd";
+import { productTypeFilter } from "../../constants";
 
 const ProductList = ({ products }) => {
+  const [filteredInfo, setFilteredInfo] = useState({});
+  const [sortedInfo, setSortedInfo] = useState({});
+
+  const changeHandler = (pagination, filters, sorter) => {
+    console.log("changeHandler filters: ", filters);
+    setFilteredInfo(filters);
+    setSortedInfo(sorter);
+  };
+
+  const setNameSort = () => {
+    setSortedInfo({
+      order: "descend",
+      columnKey: "product_name",
+    });
+  };
+
+  const clearFilters = () => {
+    setFilteredInfo(null);
+  };
+
+  const clearAll = () => {
+    setFilteredInfo(null);
+    setSortedInfo(null);
+  };
+
   const columns = [
     {
       title: "Product Name",
       dataIndex: "product_name",
       key: "productName",
-      render: (text) => <a>{text}</a>,
+      render: (text, record) => (
+        <Link href={`/products/details/${record.product_code}`}>{text}</Link>
+      ),
     },
     {
       title: "Product Code",
@@ -23,8 +50,11 @@ const ProductList = ({ products }) => {
     },
     {
       title: "Type",
-      key: "productTupe",
+      key: "productType",
       dataIndex: "product_type",
+      filters: productTypeFilter,
+      filteredValue: filteredInfo?.productType || null,
+      onFilter: (value, record) => record.product_type.includes(value),
       render: (tag) => (
         <>
           <Tag color="blue" key={tag}>
@@ -49,12 +79,16 @@ const ProductList = ({ products }) => {
     <div>
       <div>
         <p>List of All Products</p>
-        {/* <ul>
-          {products.map((product) => (
-            <ProductItem key={product.product_code} product={product} />
-          ))}
-        </ul> */}
-        <Table columns={columns} dataSource={products} />
+        <Space style={{ marginBottom: 16 }}>
+          <Button onClick={setNameSort}>Sort By Product Name</Button>
+          <Button onClick={clearFilters}>Clear filters</Button>
+          <Button onClick={clearAll}>Clear filters and sorters</Button>
+        </Space>
+        <Table
+          columns={columns}
+          dataSource={products}
+          onChange={changeHandler}
+        />
       </div>
     </div>
   );
---
+++ constants/product.js
@@ -0,0 +1,8 @@
+export const productTypeFilter = [
+  { text: "APPAREL", value: "apparel" },
+  { text: "SPORTING", value: "sporting" },
+  { text: "HEALTH", value: "health" },
+  { text: "ELECTRONIC", value: "electronic" },
+  { text: "OUTDOOR", value: "outdoor" },
+  { text: "FOOD", value: "food" },
+];
