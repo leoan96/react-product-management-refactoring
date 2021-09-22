0376ad4eac1b33e941be69b216e853bacfb560dd
Mun Kit <leoan96@hotmail.com>
2021-09-22 14:38:10 +0800

    handle backend connection error, remove deprecated ProductItem componenet

--- pages/admin/index.jsx
+++ pages/admin/index.jsx
@@ -1,8 +1,18 @@
 import React from "react";
 import ProductList from "../../components/products/ProductList";
+import Error from "next/error";
 import { enviroment } from "../../constants";
 
-const AdminPage = ({ products }) => {
+const AdminPage = ({ errorCode, products }) => {
+  if (errorCode === 500) {
+    return (
+      <Error
+        statusCode={errorCode}
+        title="Failed to connect to database"
+      ></Error>
+    );
+  }
+
   return (
     <div>
       <h1>Admin Page</h1>
@@ -13,11 +23,24 @@ const AdminPage = ({ products }) => {
 
 export default AdminPage;
 
-export async function getServerSideProps(context) {
-  const response = await fetch(enviroment.PRODUCT_SERVICE.baseUrl);
-  const products = await response.json();
+export const getServerSideProps = async ({ res }) => {
+  let products = [];
+  let errorCode = 500;
+
+  try {
+    const response = await fetch(enviroment.PRODUCT_SERVICE.baseUrl);
+    errorCode = response.ok ? 200 : +response.statusText;
+    if (errorCode !== 200) {
+      res.statusCode = errorCode;
+    }
+    products = await response.json();
+  } catch (err) {
+    console.log(err);
+    res.statusCode = errorCode;
+    console.log(res.statusCode);
+  }
 
   return {
-    props: { products }, // will be passed to the page component as props
+    props: { errorCode, products }, // will be passed to the page component as props
   };
-}
+};
